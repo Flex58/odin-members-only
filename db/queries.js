7b528ae1b87fdca533d4792bbcf729949379d02c
flex58 <164533598+Flex58@users.noreply.github.com>
2026-05-20 10:12:26 +0200

    feat: add addUser to queries

--- db/queries.js
+++ db/queries.js
@@ -11,3 +11,11 @@ exports.getUserById = async (id) => {
   const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
   return rows[0];
 };
+
+exports.addUser = async (username, password, first, last = null) => {
+  await pool.query(
+    `INSERT INTO users (username = $1, password = $2, 
+												first_name = $3, last_name = $4)`,
+    [username, password, first, last],
+  );
+};
